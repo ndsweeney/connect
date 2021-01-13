0f5582201939a4939e802794b3ccd428885b0725
ndsweeney <ndsweeney@gmail.com>
2021-01-13 14:06:17 +0000

    Add files via upload

---
+++ AzureConnect.ps1
@@ -0,0 +1,19 @@
+﻿#Connects to Azure
+
+Connect-AzAccount
+
+#Lists your Subscriptions associated with your account
+
+#ACTION - Copy the subscription you would like to access as you will need it
+
+get-azsubscription
+
+#Select the relevent subscription you want to create the resources inside of
+
+#ACTION - Paste the subscripn below where it says XXX
+
+Select-AzSubscription -SubscriptionId XXX
+
+#disconnects Account
+
+Disconnect-AzAccount
